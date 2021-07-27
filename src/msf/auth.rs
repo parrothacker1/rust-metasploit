df3c301c08f8fcb7d22fb8147250517a24417580
parrothacker1 <rajeevgoutham1@protonmail.com>
2021-07-27 04:32:40 +0000

    Push 8 part 2

--- src/msf/auth.rs
+++ src/msf/auth.rs
@@ -1,7 +1,9 @@
 #[path="../structs/mod.rs"] mod structs;
 #[path="../error.rs"] mod error;
+#[path="../connect.rs"] mod connect;
 use error::MsfError;
 use structs::auth;
+use serde_json::Value;
 
 pub struct Client {
     pub url:String,
@@ -9,12 +11,20 @@ pub struct Client {
 }
 
 pub fn logout(client: Client,out_tok:String) -> Result<bool,MsfError> {
-    let tes:bool=true;
-    Ok(tes)
+    let tes:bool;
+    let ret=connect::connect(client.url);
+    match ret {
+        Ok(retu) => {
+
 }
 pub fn token_add(client:Client,new_tok:String) -> Result<bool,MsfError> {
-    let tes:bool=true;
-    Ok(tes)
+    let tes:bool;
+    let ret:Value=connect::connect(client.url).unwrap();
+    if ret.get("result").unwrap()==&Value::from(String::from("success")) {
+        tes=true;
+    } else {
+        tes=false;
+    }
 }
 pub fn token_gen(client:Client) -> Result<String,MsfError> {
     let tes=String::new();
