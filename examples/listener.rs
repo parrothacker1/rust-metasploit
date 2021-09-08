c4833d18ef664e4fab3d7fb803df07645618114e
h4ck3r@parrot <77598345+parrothacker1@users.noreply.github.com>
2021-09-08 21:15:39 +0530

    Delete listener.rs

--- examples/listener.rs
+++
@@ -1,9 +0,0 @@
-use rust_metasploit::client::Client as client;
-struct Test {
-    host:String,
-    test:String,
-}
-fn main() {
-    let tet=client::new("127.0.0.1","msf","password",55552,true);
-    tet.print();
-}
