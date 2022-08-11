dda8b9136ac79b349bfb5b88769c82e63f6ca907
h4ck3r@parrot <rajeevgoutham1@protonmail.com>
2022-08-11 19:36:57 +0000

    Qorking on db.rs #1

--- v1.2/src/msf/blocking/db.rs
+++ v1.2/src/msf/blocking/db.rs
@@ -1,4 +1,19 @@
-pub fn hosts() {}
+#[path="../../connect.rs"] mod connect;
+#[path="../../structs/mod.rs"] mod structs;
+use crate::error::{MsfError,Error as E};
+use structs::request as req;
+use crate::client::Client;
+use serde::{Serialize,de::DeserializeOwned as DOwned};
+use rmp_serde::{Serializer,decode::Error as derror,from_read};
+
+pub fn hosts<T:DOwned>(client:Client) -> Result<T,E> {
+    let mut body=Vec::new();
+    let mut serializer=Serializer::new(&mut body);
+    let byte=req::db::hosts("db.hosts".to_string(),client.token.as_ref().unwrap().to_string());
+    byte.serialize(&mut serializer).unwrap();
+    r#return(client.url,body)
+
+}
 pub fn get_host() {}
 pub fn report_host() {}
 pub fn del_host() {}
@@ -13,16 +28,28 @@ pub fn del_vuln() {}
 pub fn report_vuln() {}
 pub fn get_vuln() {}
 
-pub fn workspaces() {}
-pub fn current_workspace() {}
-pub fn get_workspace() {}
-pub fn set_workspace() {}
-pub fn del_workspace() {}
-pub fn add_workspace() {}
+pub fn workspaces(client:Client) {
+    let mut body=Vec::new();
+    let mut serializer=Serializer::new(&mut body);
+    let byte=req::db::workspaces("db.workspaces".to_string(),client.token.as_ref().unwrap().to_string());
+    byte.serialize(&mut serializer).unwrap();
+    r#return(client.url,body);
+}
+pub fn current_workspace(client:Client) {
+    let mut body=Vec::new();
+    let mut serializer=Serializer::new(&mut body);
+    let byte=req::db::current_workspacs("db.current_workspace".to_string(),client.token.as_ref().unwrap().to_string());
+    byte.serialize(&mut serializer).unwrap();
+    r#return(client.url,body);
+}
+pub fn get_workspace(client:Client,workspace:str) {}
+pub fn set_workspace(client:Client,workspace:str) {}
+pub fn del_workspace(client:Client,workspace:str) {}
+pub fn add_workspace(client:Client,workspace:str) {}
 
 pub fn get_note() {}
 pub fn report_note() {}
-pub fn notes() {}
+pub fn notes(client,{}) {}
 pub fn del_note() {}
 
 pub fn get_client() {}
@@ -51,5 +78,33 @@ pub fn status() {}
 
 pub fn disconnect() {}
 
-fn return<T:DOwned>(body:Vec<u8>) -> Result<T,E> {
-    let con=connect::connect(body,buf);
+fn r#return<T:DOwned>(url:String,body:Vec<u8>) -> Result<T,E> {
+    let mut buf=vec![];
+    let con=connect::connect(url,body,&mut buf);
+    let new_buf=buf.clone();
+    match con {
+        Ok(_) => {
+            let ret:Result<T,derror>=from_read(new_buf.as_slice());
+            match ret {
+                Ok(val) => {
+                    Ok(val)
+                },
+                Err(_) => {
+                    let ret2:Result<MsfError,derror>=from_read(new_buf.as_slice());
+                    match ret2 {
+                        Ok(e) => {
+                            Err(E::MsfError(e))
+                        },
+                        Err(e) => {
+                            Err(E::DError(e))
+                        },
+                    }
+                },
+            }
+        },
+        Err(e) => {
+            Err(E::ConnectionError(e))
+        },
+    }
+}
+    
