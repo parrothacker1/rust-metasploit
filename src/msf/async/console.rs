674ff336f8fcc53a186f2c1f16caff8266117dcd
h4ck3r@parrot <77598345+parrothacker1@users.noreply.github.com>
2023-05-18 09:51:39 +0530

    Update console.rs

--- src/msf/async/console.rs
+++ src/msf/async/console.rs
@@ -10,10 +10,10 @@ pub async fn create<T:DOwned>(client:Client) -> Result<T,E> {
 pub async fn destroy<T:DOwned>(client:Client,consoleID:&str) -> Result<T,E> {
     console::destroy(client.clone(),consoleID)
 }
-"pub async fn list<T:DOwned>(client:Client) -> Result<T,E> {
+pub async fn list<T:DOwned>(client:Client) -> Result<T,E> {
 	console::list(client.clone())
 }
-"pub async fn write<T:DOwned>(client:Client,consoleID:&str,command:&str) -> Result<T,E> {
+pub async fn write<T:DOwned>(client:Client,consoleID:&str,command:&str) -> Result<T,E> {
     console::write(client.clone(),consoleID,command)
 }
 pub async fn read<T:DOwned>(client:Client,consoleID:&str) -> Result<T,E> {
